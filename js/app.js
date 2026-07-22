a118166de624cfab3417506790f424e945f8caf8
sam-cava <cavaliere.sam@gmail.com>
2026-07-22 23:36:11 +0200

    add: script canvas

--- js/app.js
+++ js/app.js
@@ -31,10 +31,58 @@ const observer = new IntersectionObserver((entries) => {
 
 sections.forEach(section => observer.observe(section));
 
-// ====== CLICK SUI PUNTINI: naviga alla sezione ======
+// naviga alla sezione tramite i puntini della nav
 dots.forEach(dot => {
     dot.addEventListener('click', () => {
         document.getElementById(dot.dataset.target)
             .scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+// Istallazione del canvas per l'effetto Matrix
+const matrixCanvas = document.getElementById('matrix-background');
+const ctx = matrixCanvas.getContext('2d');
+const homeSection = document.getElementById('home-section');
+let width = matrixCanvas.width = homeSection.offsetWidth;
+let height = matrixCanvas.height = homeSection.offsetHeight;
+
+window.addEventListener('resize', function () {
+    width = matrixCanvas.width = homeSection.offsetWidth;
+    height = matrixCanvas.height = homeSection.offsetHeight;
+});
+
+const fontSize = 14;
+const columnNumber = Math.floor(width / fontSize);
+const drops = Array(columnNumber).fill(0).map(() => ({
+    startHeight : Math.floor(Math.random() * height / fontSize),
+    speed : Math.floor(Math.random() * 8) + 2
+    })
+);
+let totFrames = 0;
+
+function drawMatrix() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'rgb(0, 255, 0, 0.8)';
+    ctx.font = `${fontSize}px monospace`;
+
+    drops.forEach((obj, index) => {
+        if (totFrames % obj.speed === 0) {
+            //const text = String.fromCharCode(0x30A0 + Math.floor(Math.random() * 96));
+            const alphaStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const text = alphaStr.charAt(Math.floor(Math.random() * alphaStr.length));
+            ctx.fillText(text, index * fontSize, obj.startHeight * fontSize);
+
+            if (obj.startHeight * fontSize > height && Math.random() > 0.975) {
+                obj.startHeight = 0;
+            } else {
+                obj.startHeight++;
+            }
+        }
+    });
+
+    totFrames++;
+    requestAnimationFrame(drawMatrix);
+}
+
+drawMatrix();
